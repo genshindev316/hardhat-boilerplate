158aa33a3654f77d5351f24ffdc4c27bbd435c70
genshindev316 <benjaminlee92316@gmail.com>
2024-07-12 21:39:56 +0900

    some change

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,14 @@ require("@nomicfoundation/hardhat-toolbox");
 // testing the frontend.
 require("./tasks/faucet");
 
+const INFURA_API_KEY = "0x59A38a289F70d4d9E83156E00F353527abB85B90";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    },
+  },
 };
